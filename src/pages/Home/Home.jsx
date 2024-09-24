92a904f12fab99fbd31e4ffd8219de4e66ce7f7a
Rohit <rohits8731@gmail.com>
2024-09-24 12:36:42 +0530

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className='hero'>
+            <h1>Largest <br/> Crypto MarketPlace</h1>
+            <p>Welcome to the world's largest cryptocurrency marketplace.Sign Up to explore More about the cryptos</p>
+            <form>
+                <input type='text' placeholder='Search Crypto..'/>
+                <button type='submit'>Search</button>
+            </form>
+        </div>
+    </div>
+  );
+};
+
+export default Home
